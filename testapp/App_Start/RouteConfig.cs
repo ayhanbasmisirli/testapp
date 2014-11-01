516e0312f481d11c5f5a5937fd93ccb88038a6b1
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2014-11-01 00:50:16 -0400

    home route added

--- testapp/App_Start/RouteConfig.cs
+++ testapp/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using testapp.Controllers;
 
 namespace testapp
 {
@@ -13,11 +14,8 @@ namespace testapp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "",new {controller="Home",action="index"},new []{typeof (HomeController).Namespace});
+            //default home route action index with default namespace with homecontroller
         }
     }
 }
